2f2df3a1f93a0ad3277db41ab994706628c53021
jalho <alho.jere@gmail.com>
2024-09-29 11:53:03 +0300

    Remove unused variant

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,12 +1,10 @@
 pub enum HttpError {
-    NoDelimiter,
     BadUrl(String),
     IO(std::io::ErrorKind),
 }
 impl std::fmt::Debug for HttpError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::NoDelimiter => write!(f, "NoDelimiter"),
             Self::IO(arg0) => f.debug_tuple("IO").field(arg0).finish(),
             Self::BadUrl(arg0) => f.debug_tuple("BadUrl").field(arg0).finish(),
         }
