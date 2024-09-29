11a77229364ec890e4bd1d4bb28d1e49eb19453e
jalho <alho.jere@gmail.com>
2024-09-29 12:34:12 +0300

    Remove misleading timezone identifier

--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,9 +1,10 @@
 //! Dumpster for miscellaneous stuff yet to be better categorized.
 
+/// Initialize a global logging utility.
 pub fn init_logger() -> log4rs::Handle {
     let stdout = log4rs::append::console::ConsoleAppender::builder()
         .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
-            "[{d(%Y-%m-%dT%H:%M:%S%.3fZ)}] [{l}] - {m}{n}",
+            "[{d(%Y-%m-%dT%H:%M:%S%.3f)}] [{l}] - {m}{n}",
         )))
         .build();
     let logger_config: log4rs::Config = log4rs::Config::builder()
