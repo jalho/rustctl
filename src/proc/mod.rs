de7d53680483d96b6dcc1e46eb34827ea54e9080
jalho <alho.jere@gmail.com>
2024-12-15 19:10:08 +0200

    Collect wrapped STDERR

--- src/proc/mod.rs
+++ src/proc/mod.rs
@@ -1,10 +1,26 @@
 pub struct Command {
     cmd: std::process::Command,
     cwd: std::path::PathBuf,
+    strace_output_inmem: std::fs::File,
 }
 impl Command {
     pub fn strace(cwd: &std::path::PathBuf, cmd_vec: Vec<&str>) -> Self {
-        let strace_argv: Vec<&str> = vec!["-ff", "-e", "trace=file"];
+        /* TODO: Check that strace output parsing works with "-f" like it does
+        with "-ff", and only use one of the two everywhere! */
+
+        let mut strace_argv: Vec<&str> = vec![
+            /* N.B. Using "-f" instead of "-ff" to make strace write outputs
+            into a single (inmem) file, instead of letting it make a new file
+            for each spawned subprocess. */
+            "-f",
+            "-e",
+            "trace=file",
+        ];
+
+        let (inmem_fd, inmem_path) = Command::make_inmem_file_owned();
+        strace_argv.push("-o");
+        strace_argv.push(&inmem_path);
+
         let mut cmd = std::process::Command::new("strace");
         cmd.stdout(std::process::Stdio::piped());
         cmd.stderr(std::process::Stdio::piped());
@@ -13,6 +29,7 @@ impl Command {
         return Self {
             cmd,
             cwd: cwd.clone(),
+            strace_output_inmem: inmem_fd,
         };
     }
 
@@ -27,23 +44,50 @@ impl Command {
             }
         };
         if !out.status.success() {
+            let stderr: String = match String::from_utf8(out.stderr) {
+                Ok(n) => n,
+                Err(err) => {
+                    return Err(crate::error::FatalError::new(
+                        format!("cannot decode STDERR of command as UTF-8: {:?}", self.cmd),
+                        Some(Box::new(err)),
+                    ));
+                }
+            };
             return Err(crate::error::FatalError::new(
-                format!("command exited with unsuccessful status: {:?}", self.cmd),
+                format!(
+                    "command exited with unsuccessful status: {:?}: STDERR:\n{}",
+                    self.cmd, stderr
+                ),
                 None,
             ));
         }
-        let stderr: String = match String::from_utf8(out.stderr) {
-            Ok(n) => n,
-            Err(err) => {
-                return Err(crate::error::FatalError::new(
-                    format!("cannot decode output as UTF-8 of command: {:?}", self.cmd),
-                    Some(Box::new(err)),
-                ));
-            }
-        };
+
+        let mut strace_output_inmem: String = String::new();
+        std::io::Read::read_to_string(&mut self.strace_output_inmem, &mut strace_output_inmem)
+            .unwrap(); // TODO: Don't panic!
+
         let paths: std::collections::HashSet<String> =
-            crate::misc::extract_modified_paths(&stderr, &self.cwd);
+            crate::misc::extract_modified_paths(&strace_output_inmem, &self.cwd);
         let paths: Vec<(String, u64)> = crate::misc::get_sizes(paths);
         return Ok(paths);
+    }
+
+    fn make_inmem_file_owned() -> (std::fs::File, String) {
+        let inmem_file_name: &std::ffi::CStr = &std::ffi::CString::new("strace_out.inmem").unwrap(); // TODO: Don't panic!
+
+        let inmem_fd: std::os::fd::OwnedFd = nix::sys::memfd::memfd_create(
+            inmem_file_name,
+            nix::sys::memfd::MemFdCreateFlag::empty(),
+        )
+        .unwrap(); // TODO: Don't panic!
+        let inmem_fd: i32 = std::os::fd::IntoRawFd::into_raw_fd(inmem_fd);
+
+        let inmem_file: std::fs::File = unsafe {
+            use std::os::fd::FromRawFd;
+            std::fs::File::from_raw_fd(inmem_fd)
+        };
+        let path: String = format!("/proc/self/fd/{}", inmem_fd.to_string());
+
+        return (inmem_file, path);
     }
 }
