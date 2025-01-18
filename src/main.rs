0a470a187908dd85e8032ab583d8229a96222640
jalho <alho.jere@gmail.com>
2025-01-18 12:41:48 +0200

    Plan steps

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,36 @@ fn main() {
                             std::process::exit(EXIT_ERR_DEPENDENCY_MISSING);
                         }
                     };
+                /*
+                    TODO:
+
+                    - Check if there is a SteamCMD or RustDedicated process already running:
+
+                      --> Yes: Unrecoverable error case!
+
+                    - Check whether RustDedicated is already installed
+
+                      --> Yes: Check for updates:
+                          ```
+                          $ steamcmd app_info_update 1
+                          $ steamcmd app_info_print 258550
+                          ```
+                          Then extract the build number and compare it
+                          against the value in the app manifest: `steamapps/
+                          appmanifest_258550.acf` under the server install tree.
+
+                      --> No: Install
+
+                    - Install or update Carbon modding framework
+
+                    - Install own Carbon plugins
+
+                    - Start the game server
+
+                    - Configure the running game server with Carbon to not be categorized as modded
+
+                    - Signal readiness once done: Write to some Unix domain socket, make INFO log?
+                */
             }
         },
     }
