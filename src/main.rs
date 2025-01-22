1c6b76384cf6421c33628ff49765ddda758b5aa9
jalho <alho.jere@gmail.com>
2025-01-22 20:27:18 +0200

    Remove old modules, define in main.rs for now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-mod misc;
-
 fn main() -> std::process::ExitCode {
-    crate::misc::init_logger();
+    crate::logger::init_logger();
 
     let game = crate::game::Game::init();
     game.start();
@@ -77,5 +75,45 @@ mod game {
         R(LinuxProcessId),
         /// Not running.
         NR,
+    }
+}
+
+mod logger {
+    fn make_logger_config() -> log4rs::Config {
+        let stdout: log4rs::append::console::ConsoleAppender =
+            log4rs::append::console::ConsoleAppender::builder()
+                .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
+                    "[{d(%Y-%m-%dT%H:%M:%S)}] {h([{l}])} [{t}] - {m}{n}",
+                )))
+                .build();
+
+        let logger_config: log4rs::Config = match log4rs::Config::builder()
+            .appender(log4rs::config::Appender::builder().build("stdout", Box::new(stdout)))
+            .build(
+                log4rs::config::Root::builder()
+                    .appender("stdout")
+                    .build(log::LevelFilter::Trace),
+            ) {
+            Ok(n) => n,
+            Err(_) => {
+                unreachable!("logger configuration does not depend on any input so it should be either always valid or never valid");
+            }
+        };
+
+        return logger_config;
+    }
+
+    /// Initialize a global logging utility.
+    pub fn init_logger() -> log4rs::Handle {
+        let config: log4rs::Config = make_logger_config();
+        let logger: log4rs::Handle = match log4rs::init_config(config) {
+            Ok(n) => n,
+            Err(_) => {
+                unreachable!(
+                    "logger initialization should always succeed because we only do it once"
+                );
+            }
+        };
+        return logger;
     }
 }
