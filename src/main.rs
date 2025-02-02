17b63d9141b9043427561bdc4f1f292cd81d1f53
Jere Alho <alho.jere@gmail.com>
2025-02-02 16:37:51 +0200

    Extract system module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod system;
+
 static EXIT_OK: u8 = 0;
 static EXIT_ERR_LOGGER: u8 = 42;
 static EXIT_ERR_OTHER: u8 = 43;
@@ -68,7 +70,7 @@ mod game {
     impl Game {
         pub fn start(
             exclude_from_search: Option<std::path::PathBuf>,
-        ) -> Result<Self, crate::fs::Error> {
+        ) -> Result<Self, crate::system::Error> {
             log::debug!("Determining initial state...");
             let state: S =
                 Game::determine_inital_state("RustDedicated", 258550, exclude_from_search)?;
@@ -149,12 +151,12 @@ mod game {
             executable_name: &'static str,
             steam_app_id: u32,
             exclude_from_search: Option<std::path::PathBuf>,
-        ) -> Result<S, crate::fs::Error> {
-            let installed: crate::fs::ExistingFile =
-                match crate::fs::find_single_file(executable_name, exclude_from_search) {
+        ) -> Result<S, crate::system::Error> {
+            let installed: crate::system::ExistingFile =
+                match crate::system::find_single_file(executable_name, exclude_from_search) {
                     Ok(Some(n)) => n,
                     Ok(None) => return Ok(S::NI),
-                    Err(crate::fs::Error::FileNotFound(_)) => return Ok(S::NI),
+                    Err(crate::system::Error::FileNotFound(_)) => return Ok(S::NI),
                     Err(err) => return Err(err),
                 };
 
@@ -162,8 +164,8 @@ mod game {
                 .absolute_path_parent
                 .join("steamapps")
                 .join(format!("appmanifest_{steam_app_id}.acf"));
-            let manifest: crate::fs::ExistingFile =
-                match crate::fs::ExistingFile::check(&manifest_path) {
+            let manifest: crate::system::ExistingFile =
+                match crate::system::ExistingFile::check(&manifest_path) {
                     Ok(n) => n,
                     Err(_) => return Ok(S::NI),
                 };
@@ -181,7 +183,7 @@ mod game {
                 .to_path_buf(),
             };
 
-            let running: RS = crate::fs::check_process_running(executable_name)?;
+            let running: RS = crate::system::check_process_running(executable_name)?;
 
             return Ok(S::I(updation, running));
         }
@@ -279,169 +281,6 @@ mod game {
         R(LinuxProcessId),
         /// Not running.
         NR,
-    }
-}
-
-mod fs {
-    pub fn check_process_running(name: &str) -> Result<crate::game::RS, Error> {
-        let processes: procfs::process::ProcessesIter =
-            procfs::process::all_processes().map_err(Error::ProcFsError)?;
-
-        let mut matching_pids: Vec<u32> = Vec::new();
-        for proc in processes {
-            let proc: procfs::process::Process = proc.map_err(Error::ProcFsError)?;
-            if let Ok(stat) = proc.stat() {
-                if stat.comm == name {
-                    matching_pids.push(stat.pid.try_into().expect("process ID should be a u32"));
-                }
-            }
-        }
-
-        match matching_pids.len() {
-            0 => Ok(crate::game::RS::NR),
-            1 => Ok(crate::game::RS::R(matching_pids[0])),
-            _ => Err(Error::RunningParallel(matching_pids)),
-        }
-    }
-
-    #[derive(Debug)]
-    pub enum Error {
-        /// Contains name or path (relative or absolute) of the file that was
-        /// not found, and possible associated underlying system IO error.
-        FileNotFound((std::path::PathBuf, Option<std::io::Error>)),
-        MultipleFilesFound(Vec<std::path::PathBuf>),
-        ProcFsError(procfs::ProcError),
-        RunningParallel(Vec<u32>),
-    }
-    impl std::error::Error for Error {
-        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-            match self {
-                Error::FileNotFound((_, Some(err))) => Some(err),
-                Error::FileNotFound((_, None)) => None,
-                Error::MultipleFilesFound(_) => None,
-                Error::ProcFsError(err) => Some(err),
-                Error::RunningParallel(_) => None,
-            }
-        }
-    }
-    impl std::fmt::Display for Error {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Error::FileNotFound((file, _)) => {
-                    write!(f, "file not found: {}", file.to_string_lossy())
-                }
-                Error::MultipleFilesFound(found) => {
-                    let found: Vec<String> = found
-                        .iter()
-                        .map(|n| n.to_string_lossy().into_owned())
-                        .collect::<Vec<String>>();
-                    return write!(
-                        f,
-                        "unexpected more than 1 files found: {}",
-                        found.join(", ")
-                    );
-                }
-                Error::ProcFsError(_) => write!(f, "dependency 'procfs' failed"),
-                Error::RunningParallel(vec) => {
-                    write!(
-                        f,
-                        "parallel processes running: PIDs {}",
-                        vec.iter()
-                            .map(|n| n.to_string())
-                            .collect::<Vec<String>>()
-                            .join(", ")
-                    )
-                }
-            }
-        }
-    }
-
-    pub struct ExistingFile {
-        pub file_name: std::path::PathBuf,
-        pub absolute_path_file: std::path::PathBuf,
-        pub absolute_path_parent: std::path::PathBuf,
-        pub last_change: chrono::DateTime<chrono::Utc>,
-    }
-    impl ExistingFile {
-        pub fn check(path: &std::path::Path) -> Result<Self, Error> {
-            let metadata: std::fs::Metadata = match path.metadata() {
-                Ok(n) => n,
-                Err(err) => return Err(Error::FileNotFound((path.into(), Some(err)))),
-            };
-            let absolute_path_file: std::path::PathBuf = match path.canonicalize() {
-                Ok(n) => n,
-                Err(err) => return Err(Error::FileNotFound((path.into(), Some(err)))),
-            };
-            let absolute_path_parent: std::path::PathBuf = match path.parent() {
-                Some(n) => n.into(),
-                None => unreachable!("absolute path to a file should have a parent"),
-            };
-            let file_name: std::path::PathBuf = match absolute_path_file.file_name() {
-                Some(n) => std::path::PathBuf::from(n),
-                None => unreachable!("absolute path to a file should have a file name"),
-            };
-            let ctime: i64 = std::os::unix::fs::MetadataExt::ctime(&metadata);
-            let last_change: chrono::DateTime<chrono::Utc> =
-                match chrono::DateTime::from_timestamp(ctime, 0) {
-                    Some(n) => n,
-                    None => {
-                        unreachable!("ctime of an existing file should be a valid timestamp")
-                    }
-                };
-
-            return Ok(Self {
-                absolute_path_file,
-                absolute_path_parent,
-                file_name,
-                last_change,
-            });
-        }
-    }
-
-    pub fn find_single_file(
-        executable_name: &'static str,
-        exclude_from_search: Option<std::path::PathBuf>,
-    ) -> Result<Option<ExistingFile>, Error> {
-        let mut matches: Vec<std::path::PathBuf> = Vec::new();
-
-        if let None = exclude_from_search {
-            log::debug!("Doing a full system wide search for a file named {executable_name}... This might take a while");
-        }
-        for entry in walkdir::WalkDir::new("/")
-            .into_iter()
-            .filter_entry(|e| {
-                if let Some(ref exclude_path) = exclude_from_search {
-                    if e.path().starts_with(exclude_path) {
-                        return false;
-                    }
-                }
-                true
-            })
-            .filter_map(|e| e.ok())
-        {
-            let entry: walkdir::DirEntry = entry;
-
-            if entry.file_name() == executable_name && entry.file_type().is_file() {
-                matches.push(entry.path().to_path_buf());
-            }
-
-            if matches.len() > 1 {
-                return Err(Error::MultipleFilesFound(matches));
-            }
-        }
-
-        match matches.len() {
-            0 => Err(Error::FileNotFound((executable_name.into(), None))),
-            1 => {
-                let path: std::path::PathBuf = matches
-                    .into_iter()
-                    .next()
-                    .expect("iterator of length 1 should have a first next");
-                let file: ExistingFile = ExistingFile::check(&path)?;
-                return Ok(Some(file));
-            }
-            _ => unreachable!("iterator should have length 0 or 1 at this point"),
-        }
     }
 }
 
---
+++ src/system/mod.rs
@@ -0,0 +1,163 @@
+//! System resources abstractions, such as operations with the filesystem and
+//! processes.
+
+pub fn check_process_running(name: &str) -> Result<crate::game::RS, Error> {
+    let processes: procfs::process::ProcessesIter =
+        procfs::process::all_processes().map_err(Error::ProcFsError)?;
+
+    let mut matching_pids: Vec<u32> = Vec::new();
+    for proc in processes {
+        let proc: procfs::process::Process = proc.map_err(Error::ProcFsError)?;
+        if let Ok(stat) = proc.stat() {
+            if stat.comm == name {
+                matching_pids.push(stat.pid.try_into().expect("process ID should be a u32"));
+            }
+        }
+    }
+
+    match matching_pids.len() {
+        0 => Ok(crate::game::RS::NR),
+        1 => Ok(crate::game::RS::R(matching_pids[0])),
+        _ => Err(Error::RunningParallel(matching_pids)),
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    /// Contains name or path (relative or absolute) of the file that was
+    /// not found, and possible associated underlying system IO error.
+    FileNotFound((std::path::PathBuf, Option<std::io::Error>)),
+    MultipleFilesFound(Vec<std::path::PathBuf>),
+    ProcFsError(procfs::ProcError),
+    RunningParallel(Vec<u32>),
+}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::FileNotFound((_, Some(err))) => Some(err),
+            Error::FileNotFound((_, None)) => None,
+            Error::MultipleFilesFound(_) => None,
+            Error::ProcFsError(err) => Some(err),
+            Error::RunningParallel(_) => None,
+        }
+    }
+}
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::FileNotFound((file, _)) => {
+                write!(f, "file not found: {}", file.to_string_lossy())
+            }
+            Error::MultipleFilesFound(found) => {
+                let found: Vec<String> = found
+                    .iter()
+                    .map(|n| n.to_string_lossy().into_owned())
+                    .collect::<Vec<String>>();
+                return write!(
+                    f,
+                    "unexpected more than 1 files found: {}",
+                    found.join(", ")
+                );
+            }
+            Error::ProcFsError(_) => write!(f, "dependency 'procfs' failed"),
+            Error::RunningParallel(vec) => {
+                write!(
+                    f,
+                    "parallel processes running: PIDs {}",
+                    vec.iter()
+                        .map(|n| n.to_string())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                )
+            }
+        }
+    }
+}
+
+pub struct ExistingFile {
+    pub file_name: std::path::PathBuf,
+    pub absolute_path_file: std::path::PathBuf,
+    pub absolute_path_parent: std::path::PathBuf,
+    pub last_change: chrono::DateTime<chrono::Utc>,
+}
+impl ExistingFile {
+    pub fn check(path: &std::path::Path) -> Result<Self, Error> {
+        let metadata: std::fs::Metadata = match path.metadata() {
+            Ok(n) => n,
+            Err(err) => return Err(Error::FileNotFound((path.into(), Some(err)))),
+        };
+        let absolute_path_file: std::path::PathBuf = match path.canonicalize() {
+            Ok(n) => n,
+            Err(err) => return Err(Error::FileNotFound((path.into(), Some(err)))),
+        };
+        let absolute_path_parent: std::path::PathBuf = match path.parent() {
+            Some(n) => n.into(),
+            None => unreachable!("absolute path to a file should have a parent"),
+        };
+        let file_name: std::path::PathBuf = match absolute_path_file.file_name() {
+            Some(n) => std::path::PathBuf::from(n),
+            None => unreachable!("absolute path to a file should have a file name"),
+        };
+        let ctime: i64 = std::os::unix::fs::MetadataExt::ctime(&metadata);
+        let last_change: chrono::DateTime<chrono::Utc> =
+            match chrono::DateTime::from_timestamp(ctime, 0) {
+                Some(n) => n,
+                None => {
+                    unreachable!("ctime of an existing file should be a valid timestamp")
+                }
+            };
+
+        return Ok(Self {
+            absolute_path_file,
+            absolute_path_parent,
+            file_name,
+            last_change,
+        });
+    }
+}
+
+pub fn find_single_file(
+    executable_name: &'static str,
+    exclude_from_search: Option<std::path::PathBuf>,
+) -> Result<Option<ExistingFile>, Error> {
+    let mut matches: Vec<std::path::PathBuf> = Vec::new();
+
+    if let None = exclude_from_search {
+        log::debug!("Doing a full system wide search for a file named {executable_name}... This might take a while");
+    }
+    for entry in walkdir::WalkDir::new("/")
+        .into_iter()
+        .filter_entry(|e| {
+            if let Some(ref exclude_path) = exclude_from_search {
+                if e.path().starts_with(exclude_path) {
+                    return false;
+                }
+            }
+            true
+        })
+        .filter_map(|e| e.ok())
+    {
+        let entry: walkdir::DirEntry = entry;
+
+        if entry.file_name() == executable_name && entry.file_type().is_file() {
+            matches.push(entry.path().to_path_buf());
+        }
+
+        if matches.len() > 1 {
+            return Err(Error::MultipleFilesFound(matches));
+        }
+    }
+
+    match matches.len() {
+        0 => Err(Error::FileNotFound((executable_name.into(), None))),
+        1 => {
+            let path: std::path::PathBuf = matches
+                .into_iter()
+                .next()
+                .expect("iterator of length 1 should have a first next");
+            let file: ExistingFile = ExistingFile::check(&path)?;
+            return Ok(Some(file));
+        }
+        _ => unreachable!("iterator should have length 0 or 1 at this point"),
+    }
+}
