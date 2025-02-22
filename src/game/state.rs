98a3861846c435503a027a6232a05ee5df1259b2
jalho <alho.jere@gmail.com>
2025-02-22 19:56:21 +0200

    Add notes

--- src/game/state.rs
+++ src/game/state.rs
@@ -3,7 +3,9 @@ impl NotInstalled {
     pub fn install_latest_version_from_remote(
         self,
     ) -> Result<InstalledNotRunningUpdated, std::process::ExitCode> {
-        todo!();
+        todo!(
+            "install game server using SteamCMD -- see tag state-machine-v1 for an implementation: steamcmd_exec()"
+        );
     }
 }
 
@@ -12,27 +14,29 @@ impl InstalledNotRunningNotUpdated {
     pub fn update_existing_installation_from_remote(
         self,
     ) -> Result<InstalledNotRunningUpdated, std::process::ExitCode> {
-        todo!();
+        todo!("check for game server updates and update if necessary using SteamCMD -- see tag state-machine-v1 for an implementation: steamcmd_exec()");
     }
 }
 
 pub struct InstalledNotRunningUpdated {}
 impl InstalledNotRunningUpdated {
     pub fn spawn_game_server_process(self) -> Result<RunningNotHealthy, std::process::ExitCode> {
-        todo!();
+        todo!(
+            "spawn game server process -- see tags 0.1.0, state-machine-v1 for an implementation"
+        );
     }
 }
 
 pub struct RunningNotHealthy {}
 impl RunningNotHealthy {
     pub fn healthcheck_timeout(self) -> Result<RunningHealthy, std::process::ExitCode> {
-        todo!();
+        todo!("wait for game server to become healthy somehow -- wait until timeout to see some promising log from game server?, or terminate program if cannot be determined");
     }
 }
 
 pub struct RunningHealthy {}
 impl RunningHealthy {
     pub fn wait(self) -> std::process::ExitCode {
-        return std::process::ExitCode::FAILURE;
+        todo!("run the game server until it terminates -- maybe support for graceful interrupt termination?");
     }
 }
