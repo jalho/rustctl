7022d4fe20972085125d6f210cb20aecdc85b55a
jalho <alho.jere@gmail.com>
2025-05-03 16:29:45 +0300

    Extract module system

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,11 @@
 mod constants;
 mod core;
 mod game;
+mod system;
 
 use core::SharedState;
 use std::sync::Arc;
 use tokio::{runtime, sync::Mutex};
-
-mod system {
-    use crate::{constants::INTERVAL_MONITOR_SYSTEM, core::SharedState};
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
-
-    #[derive(serde::Serialize)]
-    struct ResourceUsage {
-        process_id: (),
-        cpu: (),
-        memory: (),
-    }
-
-    #[derive(serde::Serialize)]
-    pub struct SystemState {
-        game_server: Option<ResourceUsage>,
-        game_server_installer: Option<ResourceUsage>,
-    }
-
-    impl SystemState {
-        pub fn read() -> Self {
-            Self {
-                game_server: None,
-                game_server_installer: None,
-            }
-        }
-    }
-
-    pub async fn monitor_usage(shared: Arc<Mutex<SharedState>>) {
-        let mut interval = tokio::time::interval(INTERVAL_MONITOR_SYSTEM);
-        loop {
-            interval.tick().await;
-
-            // TODO: Read system CPU, network, memory usage etc.
-
-            let usage = SystemState::read();
-
-            {
-                let mut shared = shared.lock().await;
-                shared.system = usage;
-            }
-        }
-    }
-}
 
 mod web {
     use crate::{
