17e0c55b5e7fc52fe5fbdbf6ec20a4abfe3b7c2a
jalho <alho.jere@gmail.com>
2025-05-03 16:26:32 +0300

    Extract module game

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,8 @@
+mod game;
+
 use core::SharedState;
 use std::sync::Arc;
 use tokio::{runtime, sync::Mutex};
-
-mod game {
-    use crate::{constants::INTERVAL_FETCH_GAME_STATE, core::SharedState};
-    use std::sync::Arc;
-    use tokio::sync::Mutex;
-
-    pub async fn read_state(shared: Arc<Mutex<SharedState>>) {
-        let mut interval = tokio::time::interval(INTERVAL_FETCH_GAME_STATE);
-        loop {
-            interval.tick().await;
-
-            let state = GameState::read();
-
-            {
-                let mut shared = shared.lock().await;
-                shared.game = state;
-            }
-        }
-    }
-
-    /// State of the game (obtained via RCON).
-    #[derive(serde::Serialize)]
-    pub struct GameState {
-        /// Time of day in the game world.
-        time_of_day: f64,
-
-        players: std::collections::HashMap<Identifier, Player>,
-
-        toolcupboards: std::collections::HashMap<Identifier, Toolcupboard>,
-    }
-
-    impl GameState {
-        pub fn read() -> Self {
-            // TODO: Query game state via RCON
-            Self {
-                time_of_day: 0.0,
-                players: std::collections::HashMap::new(),
-                toolcupboards: std::collections::HashMap::new(),
-            }
-        }
-    }
-
-    #[derive(serde::Serialize)]
-    struct Identifier;
-
-    #[derive(serde::Serialize)]
-    struct Location;
-
-    #[derive(serde::Serialize)]
-    struct Toolcupboard {
-        id: Identifier,
-        location: Location,
-    }
-
-    #[derive(serde::Serialize)]
-    struct Player {
-        id: Identifier,
-        location: Location,
-    }
-}
 
 mod constants {
     use std::time::Duration;
