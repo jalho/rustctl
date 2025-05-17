60bc097665623b19de7eead8c11444afa6966023
jalho <alho.jere@gmail.com>
2025-05-17 20:17:35 +0300

    Set signed cookie

--- backend/src/web/mod.rs
+++ backend/src/web/mod.rs
@@ -2,20 +2,50 @@ use crate::{
     constants::ADDR_WEB_SERVICE_LISTEN,
     core::{SharedState, handle_websocket_upgrade},
 };
-use axum::{Router, http::StatusCode, routing};
+use axum::{Router, extract::FromRef, http::StatusCode, response::IntoResponse, routing};
+use axum_extra::extract::cookie::{Cookie, Key, SignedCookieJar};
+use serde::{Deserialize, Serialize};
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tokio::{net::TcpListener, sync::Mutex};
 use tower_http::services::ServeDir;
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct ClientSession {
+    client_id: Uuid,
+}
+
+#[derive(Clone)]
+struct AppState {
+    key: Key,
+    shared: Arc<Mutex<SharedState>>,
+}
+
+impl FromRef<AppState> for Key {
+    fn from_ref(state: &AppState) -> Self {
+        state.key.clone()
+    }
+}
+
+impl FromRef<AppState> for Arc<Mutex<SharedState>> {
+    fn from_ref(state: &AppState) -> Self {
+        state.shared.clone()
+    }
+}
 
 pub async fn start(shared: Arc<Mutex<SharedState>>, web_root: PathBuf) {
+    let key = Key::generate();
+    let app_state = AppState { key, shared };
+
     let web_service = Router::new()
         .route(
             "/sock",
             routing::get(routing::get(handle_websocket_upgrade)),
         )
         .route("/favicon.ico", routing::get(routing::get(no_content)))
+        .route("/login", routing::get(login))
         .fallback_service(ServeDir::new(web_root).append_index_html_on_directories(true))
-        .with_state(shared);
+        .with_state(app_state);
 
     let listener = TcpListener::bind(ADDR_WEB_SERVICE_LISTEN).await.unwrap();
 
@@ -29,4 +59,15 @@ pub async fn start(shared: Arc<Mutex<SharedState>>, web_root: PathBuf) {
 
 async fn no_content() -> StatusCode {
     StatusCode::NO_CONTENT
+}
+
+async fn login(jar: SignedCookieJar) -> impl IntoResponse {
+    let session = ClientSession {
+        client_id: Uuid::new_v4(),
+    };
+    let session_json = serde_json::to_string(&session).unwrap();
+
+    let new_jar = jar.add(Cookie::new("session", session_json));
+
+    (StatusCode::OK, new_jar)
 }
