6d3653038089cbba016374dc9febad65d932d061
jalho <alho.jere@gmail.com>
2025-05-17 13:12:51 +0300

    Make send_and_receive_messages Client's method

--- backend/src/core/mod.rs
+++ backend/src/core/mod.rs
@@ -7,98 +7,22 @@ use axum::{
     response::IntoResponse,
 };
 use futures::{SinkExt, StreamExt};
-use std::{collections::HashMap, net::SocketAddr, path::PathBuf, sync::Arc};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
 use tokio::sync::{Mutex, MutexGuard};
 
 pub async fn handle_websocket_upgrade(
-    shared: State<Arc<Mutex<SharedState>>>,
+    State(state): State<Arc<Mutex<SharedState>>>,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     ws: WebSocketUpgrade,
 ) -> impl IntoResponse {
-    {
-        let mut shared_locked: MutexGuard<SharedState> = shared.lock().await;
-        shared_locked.clients.insert(addr, Client::new(addr));
-    }
-    ws.on_upgrade(move |sock| send_and_receive_messages(shared, addr, sock))
-}
-
-async fn send_and_receive_messages(
-    shared: State<Arc<Mutex<SharedState>>>,
-    addr: SocketAddr,
-    sock: WebSocket,
-) {
-    let (mut sock_tx, mut sock_rx) = StreamExt::split(sock);
-
-    let shared_rx: Arc<Mutex<SharedState>> = Arc::clone(&shared);
-    let jh_rx_cmd = tokio::task::Builder::new()
-        .name("recv_commands")
-        .spawn(async move {
-            loop {
-                let recv = StreamExt::next(&mut sock_rx).await;
-
-                match recv {
-                    Some(Ok(Message::Text(msg))) => {
-                        let command = Command::new(&msg.to_string());
-
-                        // TODO: Do a state transition based on the received command
-                        println!("TODO: Transition state: {command:?}");
-                    }
-                    _ => {
-                        break;
-                    }
-                }
-            }
-        })
-        .unwrap();
-
-    let shared_tx: Arc<Mutex<SharedState>> = Arc::clone(&shared);
-    let jh_tx_state = tokio::task::Builder::new()
-        .name("send_state")
-        .spawn(async move {
-            let mut interval = tokio::time::interval(INTERVAL_SYNC_CLIENT);
-            loop {
-                interval.tick().await;
-
-                {
-                    let shared_locked: MutexGuard<SharedState> = shared_tx.lock().await;
-                    let sent = SinkExt::send(&mut sock_tx, shared_locked.serialize()).await;
-                    if sent.is_err() {
-                        break;
-                    }
-                }
-            }
-        })
-        .unwrap();
-
-    _ = jh_rx_cmd.await;
-    _ = jh_tx_state.await;
-
-    {
-        let mut shared = shared.lock().await;
-        shared.clients.remove(&addr);
-    }
-}
-
-pub struct WebAssets {
-    pub abs_path_index_html: String,
-}
-
-trait ExistingFile {
-    fn to_absolute_path(&self, root: &PathBuf) -> String;
-}
-
-impl ExistingFile for &str {
-    fn to_absolute_path(&self, root: &PathBuf) -> String {
-        let mut path = root.to_owned();
-        path.push(self);
-        path = path.canonicalize().unwrap();
-        path.to_str().unwrap().to_owned()
-    }
+    ws.on_upgrade(move |sock| {
+        let client = Client::new(addr, sock, Arc::clone(&state));
+        client.send_and_receive_messages()
+    })
 }
 
 #[derive(serde::Serialize)]
 struct ClientState {
-    pub clients: HashMap<SocketAddr, Client>,
     pub game: GameState,
     pub system: SystemState,
 }
@@ -106,7 +30,6 @@ struct ClientState {
 impl From<&SharedState> for ClientState {
     fn from(value: &SharedState) -> Self {
         Self {
-            clients: value.clients.clone(),
             game: value.game.clone(),
             system: value.system.clone(),
         }
@@ -114,7 +37,6 @@ impl From<&SharedState> for ClientState {
 }
 
 pub struct SharedState {
-    pub clients: HashMap<SocketAddr, Client>,
     pub game: GameState,
     pub system: SystemState,
 }
@@ -122,7 +44,6 @@ pub struct SharedState {
 impl SharedState {
     pub fn init() -> Arc<Mutex<Self>> {
         Arc::new(Mutex::new(Self {
-            clients: HashMap::new(),
             game: GameState::read(),
             system: SystemState::read(),
         }))
@@ -148,14 +69,63 @@ impl Command {
     }
 }
 
-#[derive(serde::Serialize, Clone)]
 pub struct Client {
     addr: SocketAddr,
+    sock: WebSocket,
+    shared: Arc<Mutex<SharedState>>,
 }
 
 impl Client {
-    pub fn new(addr: SocketAddr) -> Self {
-        Self { addr }
+    pub fn new(addr: SocketAddr, sock: WebSocket, shared: Arc<Mutex<SharedState>>) -> Self {
+        Self { addr, sock, shared }
+    }
+
+    async fn send_and_receive_messages(self) {
+        let (mut sock_tx, mut sock_rx) = StreamExt::split(self.sock);
+
+        let shared_rx: Arc<Mutex<SharedState>> = Arc::clone(&self.shared);
+        let jh_rx_cmd = tokio::task::Builder::new()
+            .name("recv_commands")
+            .spawn(async move {
+                loop {
+                    let recv = StreamExt::next(&mut sock_rx).await;
+
+                    match recv {
+                        Some(Ok(Message::Text(msg))) => {
+                            let command = Command::new(&msg.to_string());
+
+                            // TODO: Do a state transition based on the received command
+                            println!("TODO: Transition state: {command:?}");
+                        }
+                        _ => {
+                            break;
+                        }
+                    }
+                }
+            })
+            .unwrap();
+
+        let shared_tx: Arc<Mutex<SharedState>> = Arc::clone(&self.shared);
+        let jh_tx_state = tokio::task::Builder::new()
+            .name("send_state")
+            .spawn(async move {
+                let mut interval = tokio::time::interval(INTERVAL_SYNC_CLIENT);
+                loop {
+                    interval.tick().await;
+
+                    {
+                        let shared_locked: MutexGuard<SharedState> = shared_tx.lock().await;
+                        let sent = SinkExt::send(&mut sock_tx, shared_locked.serialize()).await;
+                        if sent.is_err() {
+                            break;
+                        }
+                    }
+                }
+            })
+            .unwrap();
+
+        _ = jh_rx_cmd.await;
+        _ = jh_tx_state.await;
     }
 }
 
