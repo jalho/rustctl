eb6f5fa184fbf7f4e17dece53450e0099e9169c0
jalho <alho.jere@gmail.com>
2025-05-24 21:41:47 +0300

    Disconnect WebSocket on unmount

--- frontend/src/logic/ConnectWebSocket.tsx
+++ frontend/src/logic/ConnectWebSocket.tsx
@@ -1,22 +1,22 @@
-import * as libredux from "react-redux";
+import * as libreact from "react";
+import * as librredux from "react-redux";
 import * as libstore from "../store/_mod";
-import type * as libreact from "react";
+import type * as libredux from "redux";
 import type * as libstatus from "../store/status";
 import type * as libsync from "../store/sync";
+
+let SOCKET: null | WebSocket;
 
 type Props = { loggedIn: libstatus.LoggedIn, urlWs: URL };
 
 const ConnectWebSocket = (props: Props): libreact.ReactElement => {
-  const state: libsync.State = libredux.useSelector<libstore.RootState, libsync.State>((s) => {
+  const state: libsync.State = librredux.useSelector<libstore.RootState, libsync.State>((s) => {
     return s.sync;
   });
-  const dispatch = libredux.useDispatch();
-  /*
-   *  dispatch(libstore.actions.status.setOffline());
-   */
+  const dispatch = librredux.useDispatch();
+  libreact.useEffect(connectWebSocket(dispatch, props.urlWs), [/* on mount */]);
 
   if (state === null) {
-    const ws = new WebSocket(props.urlWs);
     return (
       <>
         Session ID: {props.loggedIn.sessionId} -- Connecting WebSocket...
@@ -33,3 +33,24 @@ const ConnectWebSocket = (props: Props): libreact.ReactElement => {
 };
 
 export default ConnectWebSocket;
+
+function connectWebSocket(
+  dispatch: libreact.Dispatch<libredux.UnknownAction>,
+  urlWs: URL,
+) {
+  return function() {
+    console.debug("TODO: Connect WebSocket");
+    SOCKET = new WebSocket(urlWs);
+    SOCKET.addEventListener("message", function handleMessage(event) {
+      console.debug("TODO: Store in Redux", event);
+    });
+    return disconnectWebSocket;
+  }
+}
+
+function disconnectWebSocket() {
+  if (SOCKET) {
+    SOCKET.close();
+  }
+  console.debug("TODO: Disconnect WebSocket");
+}
