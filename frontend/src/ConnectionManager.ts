d231b281d9bb110a0f0be8bab959d935c67dc4bb
jalho <alho.jere@gmail.com>
2025-05-29 16:51:11 +0300

    Document Fetch API error

--- frontend/src/ConnectionManager.ts
+++ frontend/src/ConnectionManager.ts
@@ -11,6 +11,17 @@ class ConnectionManager {
       response = await fetch(this.#url_status);
       response_instant = new Date().toISOString();
     } catch (err) {
+      /**
+       * Fetch API error: Not necessarily "offline", but could be. A
+       * non-exhaustive list of some possible causes:
+       * - not connected to the internet (the "offline" case)
+       * - DNS issues
+       * - CORS issues
+       * - TLS issues
+       *
+       * In other words, we just cannot reliably detect what's wrong exactly.
+       * That's just a downside of the platform (web) I suppose.
+       */
       const error = err as DOMException;
       Store.dispatch(SessionSlice.actions.set_error({
         name: display(error.name),
