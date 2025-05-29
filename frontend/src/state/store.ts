da564bc2da642d6b58c7ccc1f6ea6cb7de37892f
jalho <alho.jere@gmail.com>
2025-05-29 21:00:02 +0300

    Remove dummy state

--- frontend/src/state/store.ts
+++ frontend/src/state/store.ts
@@ -1,10 +1,8 @@
 import * as reduxjs_toolkit from '@reduxjs/toolkit'
-import counter from "./slices/counter";
 import session from "./slices/session";
 
 const redux_store = reduxjs_toolkit.configureStore({
   reducer: {
-    counter: counter.reducer,
     session: session.reducer,
   },
 });
