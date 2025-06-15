e95a3bc9384529b8c2636cc001603af08444dd56
jalho <alho.jere@gmail.com>
2025-06-15 18:00:30 +0300

    Check if dependency SteamCMD is already running

--- rustctl-backend/src/game/mod.rs
+++ rustctl-backend/src/game/mod.rs
@@ -1,4 +1,5 @@
 use crate::core::CrossTasksSharedState;
+use proc::Dependency;
 use rustctl_common::{
     snapshot::{Game, GameState, StateTransitionInitiator},
     state_machine::{NotRunning, ShutdownInProgress, StartupInProgress},
@@ -42,9 +43,25 @@ pub trait GameStateMachine {
 
 impl GameStateMachine for GameState {
     async fn update_and_launch(&mut self, initiator: StateTransitionInitiator) {
-        log::debug!("TODO: Check if SteamCMD or RustDedicated is already running");
+        log::debug!("Checking if SteamCMD or RustDedicated is already running...");
+        let mut command = tokio::process::Command::new(Dependency::pgrep.get_absolute_path());
+        let command = command.current_dir("/");
+        let command = command.args(vec![Dependency::steamcmd.get_executable_name()]);
+        let output = command.spawn().unwrap().wait_with_output().await.unwrap();
+        log::debug!("\t{command:?}");
+        log::debug!("\t{output:?}");
+        if output.status.success() {
+            // TODO: Implement non-recoverable error: Make error log and terminate the program!
+            log::error!(
+                "Cannot update and launch game: Dependency already running: {dependency}",
+                dependency = Dependency::steamcmd
+            );
+        }
+
         log::debug!("TODO: Install or update RustDedicatedd using SteamCMD");
+
         log::debug!("TODO: Launch RustDedicated");
+
         self.game = Game::NotRunning(NotRunning {});
         self.last_state_transition_at = chrono::Utc::now();
         self.last_state_transition_inititated_by = initiator;
@@ -107,6 +124,41 @@ impl GameStateMachine for GameState {
 }
 
 mod proc {
+
+    #[allow(non_camel_case_types)]
+    pub enum Dependency {
+        pgrep,
+        RustDedicated,
+        steamcmd,
+    }
+
+    impl Dependency {
+        pub fn get_absolute_path(&self) -> std::path::PathBuf {
+            match self {
+                Dependency::pgrep => std::path::Path::new(EXEC_ABS_PGREP).to_path_buf(),
+                Dependency::RustDedicated => std::path::Path::new(EXEC_ABS_RDS).to_path_buf(),
+                Dependency::steamcmd => std::path::Path::new(EXEC_ABS_STEAMCMD).to_path_buf(),
+            }
+        }
+
+        pub fn get_executable_name(&self) -> String {
+            let absolute_path: std::path::PathBuf = self.get_absolute_path();
+            let executable_name: &std::ffi::OsStr = absolute_path.file_name().unwrap();
+            return executable_name.to_string_lossy().to_owned().to_string();
+        }
+    }
+
+    impl std::fmt::Display for Dependency {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(
+                f,
+                "\"{}\" (\"{}\")",
+                self.get_executable_name(),
+                self.get_absolute_path().to_string_lossy(),
+            )
+        }
+    }
+
     /// Absolute path of the `steamcmd` executable, i.e. the game server installer.
     const EXEC_ABS_STEAMCMD: &'static str = "/home/jka/probe/mock-steamcmd";
     /// Absolute path of the `RustDedicated` executable, i.e. the game server.
