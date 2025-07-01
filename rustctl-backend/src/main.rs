02d81eac7c7696159f0ea2c78c0cdd84fa77e90c
jalho <alho.jere@gmail.com>
2025-07-02 01:11:16 +0300

    Receive messages

--- rustctl-backend/src/main.rs
+++ rustctl-backend/src/main.rs
@@ -1,11 +1,10 @@
 /*
- * TODO: Rewrite in terms of the "actor pattern" (a concurrency pattern): There
- *       should be _actors_ that own their stuff (such as I/O resources), and
- *       that perform work in coroutines (alias "background tasks"), and that
- *       may communicate with other actors via various _channels_. The main
- *       components of the program should all be actors, and the program's
- *       main functionality should be implemented by arranging channels between
- *       actors.
+ * Rewrite in terms of the "actor pattern" (a concurrency pattern): There should
+ * be _actors_ that own their stuff (such as I/O resources), and that perform
+ * work in coroutines (alias "background tasks"), and that may communicate
+ * with other actors via various _channels_. The main components of the
+ * program should all be actors, and the program's main functionality should be
+ * implemented by arranging channels between actors.
  */
 fn main() {
     let stage = Actor::<Stage, DownstreamClientMessage>::new(Stage::new());
@@ -21,7 +20,7 @@ fn main() {
 
     runtime.block_on(async {
         let coroutine_web = tokio::spawn(async move {
-            let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+            let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
                 .await
                 .unwrap();
             axum::serve(tcp_listener, router).await.unwrap();
@@ -35,13 +34,37 @@ async fn websocket_handler(
     axum::extract::State(stage): axum::extract::State<ActorHandle<DownstreamClientMessage>>,
 ) -> axum::response::Response {
     ws.on_upgrade(async |socket| {
-        let _socket: axum::extract::ws::WebSocket = socket;
-        let _stage = stage;
+        let mut socket: axum::extract::ws::WebSocket = socket;
+        let stage: ActorHandle<DownstreamClientMessage> = stage;
+
+        'recv_messages: while let Some(Ok(message)) =
+            futures_util::StreamExt::next(&mut socket).await
+        {
+            let msg_raw: axum::extract::ws::Message = message;
+            let msg_valid: DownstreamClientMessage = match (&msg_raw).try_into() {
+                Ok(n) => n,
+                Err(err) => {
+                    eprintln!("invalid message from downstream client: {err:?}: {msg_raw:?}");
+                    continue 'recv_messages;
+                }
+            };
+            if let Err(err) = stage.send(msg_valid).await {
+                eprintln!("could not send message from downstream client to stage: {err:?}");
+                continue 'recv_messages;
+            };
+        }
     })
 }
 
-#[derive(Clone)]
-struct DownstreamClientMessage;
+#[derive(Clone, Debug)]
+enum DownstreamClientMessage {}
+impl TryFrom<&axum::extract::ws::Message> for DownstreamClientMessage {
+    type Error = ();
+
+    fn try_from(value: &axum::extract::ws::Message) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 
 struct Actor<Resource, Message> {
     managed_resource: Resource,
@@ -71,6 +94,13 @@ struct ActorHandle<Message> {
 impl<Message> ActorHandle<Message> {
     pub fn new(tx: tokio::sync::mpsc::Sender<Message>) -> Self {
         Self { tx }
+    }
+
+    pub async fn send(
+        &self,
+        message: Message,
+    ) -> Result<(), tokio::sync::mpsc::error::TrySendError<Message>> {
+        self.tx.try_send(message)
     }
 }
 
