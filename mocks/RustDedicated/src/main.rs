52bb85131909b3b5eb8b54fe9e0b397641d4b904
jalho <alho.jere@gmail.com>
2025-07-14 14:44:23 +0300

    Adjust mock

--- mocks/RustDedicated/src/main.rs
+++ mocks/RustDedicated/src/main.rs
@@ -12,7 +12,7 @@ fn main() -> std::process::ExitCode {
     };
 
     let _runtime_done = runtime.block_on(async {
-        let summary = tokio::join!(launch_game(std::time::Duration::from_secs(30)));
+        let summary = tokio::join!(launch_game(std::time::Duration::from_secs(5)));
         return summary;
     });
 
