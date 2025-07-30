12390800196268b5fe4d5c6576d3616559cb577b
jalho <alho.jere@gmail.com>
2025-07-30 19:47:29 +0300

    Mock slow shutdown

--- mocks/RustDedicated/src/main.rs
+++ mocks/RustDedicated/src/main.rs
@@ -11,15 +11,18 @@ fn main() -> std::process::ExitCode {
         }
     };
 
-    let _runtime_done = runtime.block_on(async {
-        let summary = tokio::join!(launch_game(std::time::Duration::from_secs(60)));
+    let runtime_done: std::process::ExitCode = runtime.block_on(async {
+        let summary = tokio::select!(
+            code = launch_game(std::time::Duration::from_secs(20)) => code,
+            code = wait_signal() => code,
+        );
         summary
     });
 
-    std::process::ExitCode::FAILURE
+    runtime_done
 }
 
-async fn launch_game(duration: std::time::Duration) {
+async fn launch_game(duration: std::time::Duration) -> std::process::ExitCode {
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     println!("SteamServer Initialized");
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
@@ -31,4 +34,30 @@ async fn launch_game(duration: std::time::Duration) {
     println!("Sleeping {duration:?}");
     tokio::time::sleep(duration).await;
     println!("Done sleeping");
+
+    std::process::ExitCode::FAILURE
+}
+
+async fn wait_signal() -> std::process::ExitCode {
+    let mut sigint =
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt()).unwrap();
+    let mut sigterm =
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()).unwrap();
+    tokio::select! {
+        _ = sigint.recv() => {
+            println!("SIGINT");
+
+            println!("Sleeping a bit...");
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            println!("Done!");
+        },
+        _ = sigterm.recv() => {
+            println!("SIGTERM");
+
+            println!("Sleeping a bit...");
+            tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+            println!("Done!");
+        },
+    };
+    std::process::ExitCode::SUCCESS
 }
