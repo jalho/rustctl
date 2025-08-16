dc4a83b6b4ad74a6739653837aeb6a91302111a8
jalho <alho.jere@gmail.com>
2025-08-16 18:19:07 +0300

    Activate termination on signal

--- server/src/actors/terminator.rs
+++ server/src/actors/terminator.rs
@@ -8,14 +8,11 @@ impl Terminator {
         ctoken: tokio_util::sync::CancellationToken,
         rx_activate: tokio::sync::mpsc::Receiver<Activator>,
     ) -> Self {
-        Self {
-            ctoken,
-            rx_activate,
-        }
+        Self { ctoken, rx_activate }
     }
 
-    pub async fn work(self) -> Summary {
-        let sigint: tokio::signal::unix::Signal =
+    pub async fn work(mut self) -> Summary {
+        let mut sigint: tokio::signal::unix::Signal =
             match tokio::signal::unix::signal(tokio::signal::unix::SignalKind::interrupt()) {
                 Ok(n) => n,
                 Err(err) => {
@@ -23,6 +20,42 @@ impl Terminator {
                     return Summary {};
                 }
             };
+
+        let mut sigterm: tokio::signal::unix::Signal =
+            match tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate()) {
+                Ok(n) => n,
+                Err(err) => {
+                    log::error!("Failed to create signal listener for SIGTERM: {err}");
+                    return Summary {};
+                }
+            };
+
+        let job = async {
+            tokio::select! {
+                _ = sigint.recv() => {
+                    log::debug!("Termination token activation by SIGINT");
+                    self.ctoken.cancel();
+                },
+                _ = sigterm.recv() => {
+                    log::debug!("Termination token activation by SIGTERM");
+                    self.ctoken.cancel();
+                },
+                received = self.rx_activate.recv() => {
+                    match received {
+                        Some(activator) => {
+                            log::debug!("Termination token activation by {activator:?}");
+                            self.ctoken.cancel();
+                        },
+                        None => {
+                            log::error!("BUG: Termination token activation channel closed while Terminator is still working!");
+                            self.ctoken.cancel();
+                        },
+                    }
+                }
+            }
+        };
+        let _done: () = job.await;
+
         return Summary {};
     }
 }
@@ -30,4 +63,5 @@ impl Terminator {
 pub struct Summary {}
 
 /// Which actor is activating the global termination signal.
+#[derive(Debug)]
 pub enum Activator {}
