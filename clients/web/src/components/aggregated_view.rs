3a968a1758889f94d8fd29c04d5c4f4f9b2bb4d8
jalho <alho.jere@gmail.com>
2025-09-06 14:42:19 +0300

    Add placeholder

--- clients/web/src/components/aggregated_view.rs
+++ clients/web/src/components/aggregated_view.rs
@@ -2,14 +2,20 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn AggregatedView(state: crate::state::State) -> Element {
-    rsx! {
-        div {
-            for (steam_id , resources) in &state.aggregated {
-                div { style: "margin-bottom: 10px;",
-                    h3 { "Player {steam_id}" }
-                    ul {
-                        for (resource , amount) in resources {
-                            li { "{resource:?}: {amount}" }
+    if state.aggregated.is_empty() {
+        rsx! {
+            p { "Nothing." }
+        }
+    } else {
+        rsx! {
+            div {
+                for (steam_id , resources) in &state.aggregated {
+                    div { style: "margin-bottom: 10px;",
+                        h3 { "Player {steam_id}" }
+                        ul {
+                            for (resource , amount) in resources {
+                                li { "{resource:?}: {amount}" }
+                            }
                         }
                     }
                 }
