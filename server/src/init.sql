0ca9682362ba207c937fdc52edd55f3403d63b57
jalho <alho.jere@gmail.com>
2025-09-10 19:52:15 +0300

    Define FK

--- server/src/init.sql
+++ server/src/init.sql
@@ -2,32 +2,29 @@
  * TABLES.
  */
 CREATE TABLE steam_ids (
-  id        TEXT PRIMARY KEY, -- UUIDv4
-  steam_id  INTEGER NOT NULL  -- 17-digit unsigned integer
+    id              TEXT PRIMARY KEY, -- UUIDv4
+    steam_id        INTEGER NOT NULL  -- 17-digit unsigned integer
 );
 CREATE TABLE users (
-  id          TEXT PRIMARY KEY, -- UUIDv4
-  privileged  BOOLEAN NOT NULL
+    id              TEXT PRIMARY KEY, -- UUIDv4
+    privileged      BOOLEAN NOT NULL,
+    FOREIGN KEY(id) REFERENCES steam_ids(id)
 );
 
 /*
  * DATA.
  */
-INSERT INTO
-    steam_ids(
-        id,
-        steam_id
-    )
-    VALUES(
-        '00000000-0000-0000-0000-000000000000',
-        76561198135242017
-    );
-INSERT INTO
-    users(
-        id,
-        privileged
-    )
-    VALUES(
-        '11111111-1111-1111-1111-111111111111',
-        1
-    );
+INSERT INTO steam_ids (
+    id,
+    steam_id
+) VALUES (
+    '00000000-0000-0000-0000-000000000000',
+    76561198135242017
+);
+INSERT INTO users (
+    id,
+    privileged
+) VALUES (
+    '00000000-0000-0000-0000-000000000000',
+    1
+);
