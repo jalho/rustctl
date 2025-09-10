d0605f3286275064d924949e27aade7e10a98e6b
jalho <alho.jere@gmail.com>
2025-09-10 19:57:18 +0300

    Rerelate

--- server/src/init.sql
+++ server/src/init.sql
@@ -1,30 +1,18 @@
 /*
  * TABLES.
  */
-CREATE TABLE steam_ids (
-    id              TEXT PRIMARY KEY, -- UUIDv4
-    steam_id        INTEGER NOT NULL  -- 17-digit unsigned integer
-);
 CREATE TABLE users (
-    id              TEXT PRIMARY KEY, -- UUIDv4
-    privileged      BOOLEAN NOT NULL,
-    FOREIGN KEY(id) REFERENCES steam_ids(id)
+    id                   TEXT PRIMARY KEY, -- UUIDv4
+    privileged           BOOLEAN NOT NULL
+);
+CREATE TABLE alt_ids (
+    id                   TEXT PRIMARY KEY, -- UUIDv4
+    steam_id             INTEGER NOT NULL, -- 17-digit unsigned integer
+    user_id              TEXT, -- UUIDv4
+    FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
 /*
  * DATA.
  */
-INSERT INTO steam_ids (
-    id,
-    steam_id
-) VALUES (
-    '00000000-0000-0000-0000-000000000000',
-    76561198135242017
-);
-INSERT INTO users (
-    id,
-    privileged
-) VALUES (
-    '00000000-0000-0000-0000-000000000000',
-    1
-);
+-- TODO!
