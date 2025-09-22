e18c45401c51773d7897573073e3aaf54f81236c
jalho <alho.jere@gmail.com>
2025-09-22 21:47:48 +0300

    Add methods

--- server/src/actors/database/mod.rs
+++ server/src/actors/database/mod.rs
@@ -27,34 +27,13 @@ impl Database {
          *       given in CLI args.
          */
         dbg!(populate_privileged_users);
-        {
-            let mut statement = match connection.prepare("SELECT id FROM users") {
-                Ok(n) => n,
-                Err(err) => {
-                    dbg!(&err);
-                    log::error!("{err}");
-                    return Err(std::process::ExitCode::FAILURE);
-                }
-            };
-
-            let selection = match statement.query_map([], |row| Ok(User { id: row.get(0)? })) {
-                Ok(n) => n,
-                Err(err) => {
-                    log::error!("{err}");
-                    return Err(std::process::ExitCode::FAILURE);
-                }
-            };
-            for selected in selection {
-                let user: User = match selected {
-                    Ok(n) => n,
-                    Err(err) => {
-                        log::error!("{err}");
-                        return Err(std::process::ExitCode::FAILURE);
-                    }
-                };
-                dbg!(user);
-            }
-        }
+        let users: Vec<User> = match Self::select_all_privileged_users(&connection) {
+            Ok(n) => n,
+            Err(err) => {
+                todo!();
+            },
+        };
+        dbg!(users);
 
         Ok(Self { connection })
     }
@@ -62,9 +41,40 @@ impl Database {
     fn check_version(connection: &rusqlite::Connection) -> Result<String, rusqlite::Error> {
         connection.query_row("SELECT sqlite_version()", [], |row| row.get(0))
     }
+
+    fn select_all_privileged_users(connection: &rusqlite::Connection) -> Result<Vec<User>, rusqlite::Error> {
+        let mut statement: rusqlite::Statement = connection.prepare("SELECT id FROM users")?;
+
+        let selection = statement.query_map([], |row| Ok(User { id: row.get(0)? }))?;
+
+        let mut users: Vec<User> = Vec::new();
+        for selected in selection {
+            let user: User = selected?;
+            users.push(user);
+        }
+
+        return Ok(users);
+    }
+
+    fn create_tables(connection: &rusqlite::Connection) -> Result<(), rusqlite::Error> {
+        let _created: () = connection.execute_batch(TABLES)?;
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
 struct User {
     id: String,
 }
+
+const TABLES: &'static str = r#"CREATE TABLE users (
+    id                   TEXT NOT NULL PRIMARY KEY,
+    privileged_at_utc    DATETIME NULL
+);
+CREATE TABLE alt_ids (
+    id                   TEXT NOT NULL PRIMARY KEY,
+    steam_id             INTEGER NOT NULL,
+    user_id              TEXT NOT NULL,
+    created_at_utc       DATETIME NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);"#;
