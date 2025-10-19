4b69b363e2aebfba1c851ed0fe585d9f7393febd
Jere Alho <alho.jere@gmail.com>
2025-10-19 22:15:11 +0300

    Configure keyboard layout

--- create-cloud-init-seed.sh
+++ create-cloud-init-seed.sh
@@ -9,27 +9,30 @@ mkdir -p "$WORKDIR"
 cat > "$WORKDIR/user-data" <<EOF
 #cloud-config
 users:
-  - name: foo2
+  - name: foo4
     sudo: ALL=(ALL) NOPASSWD:ALL
     shell: /bin/bash
 chpasswd:
   expire: False
   list: |
-    foo2:bar
+    foo4:bar
 ssh_pwauth: True
 packages:
   - kbd
   - console-data
+  - console-setup
 runcmd:
-  - loadkeys fi
+  - echo "KEYMAP=fi" > /etc/vconsole.conf
+  - echo 'XKBLAYOUT="fi"' > /etc/default/keyboard
+  - setupcon -k --force
+  - systemctl restart systemd-vconsole-setup.service || true
 EOF
 
 cat > "$WORKDIR/meta-data" <<EOF
 instance-id: iid-local01
-local-hostname: foo2-vbox
+local-hostname: foo4-vbox
 EOF
 
 cloud-localds "$OUTPUT_ISO" "$WORKDIR/user-data" "$WORKDIR/meta-data"
 
 echo "Seed ISO created: $OUTPUT_ISO"
-
