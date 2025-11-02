83e34f0e2552cf17685317b708cdd1885d004777
jalho <alho.jere@gmail.com>
2025-11-02 14:03:40 +0200

    Add DecodeHex::to_decoded

--- server/src/steam.rs
+++ server/src/steam.rs
@@ -633,6 +633,29 @@ fn test_from_vdf_appmanifest() {
     let _buildid: BuildID = BuildID::from_vdf_appmanifest(input).unwrap();
 }
 
+trait DecodeHex {
+    fn to_decoded(&self) -> Result<Vec<u8>, String>;
+}
+
+impl DecodeHex for &str {
+    fn to_decoded(&self) -> Result<Vec<u8>, String> {
+        self.trim()
+            .as_bytes()
+            .chunks(2)
+            .map(|chunk| {
+                let hex_byte: &str = match std::str::from_utf8(chunk) {
+                    Ok(n) => n,
+                    Err(err) => return Err(err.to_string()),
+                };
+                match u8::from_str_radix(hex_byte, 16) {
+                    Ok(n) => Ok(n),
+                    Err(err) => return Err(err.to_string()),
+                }
+            })
+            .collect()
+    }
+}
+
 trait DisplayHex {
     fn to_hex_encoded_string(&self) -> String;
 }
