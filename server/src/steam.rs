8e4042638d9af3651e1e4614a0cb8cc20eade4f0
jalho <alho.jere@gmail.com>
2025-11-02 14:19:59 +0200

    Define to_decoded for test only

--- server/src/steam.rs
+++ server/src/steam.rs
@@ -633,29 +633,6 @@ fn test_from_vdf_appmanifest() {
     let _buildid: BuildID = BuildID::from_vdf_appmanifest(input).unwrap();
 }
 
-trait DecodeHex {
-    fn to_decoded(&self) -> Result<Vec<u8>, String>;
-}
-
-impl DecodeHex for &str {
-    fn to_decoded(&self) -> Result<Vec<u8>, String> {
-        self.trim()
-            .as_bytes()
-            .chunks(2)
-            .map(|chunk| {
-                let hex_byte: &str = match std::str::from_utf8(chunk) {
-                    Ok(n) => n,
-                    Err(err) => return Err(err.to_string()),
-                };
-                match u8::from_str_radix(hex_byte, 16) {
-                    Ok(n) => Ok(n),
-                    Err(err) => Err(err.to_string()),
-                }
-            })
-            .collect()
-    }
-}
-
 trait DisplayHex {
     fn to_hex_encoded_string(&self) -> String;
 }
@@ -666,10 +643,36 @@ impl DisplayHex for Vec<u8> {
     }
 }
 
-#[test]
-fn test_from_vdf_steamcmd_contaminated_2() {
-    let hex: &'static str = include_str!("../../TODO/sample.hex");
-    let decoded: Vec<u8> = hex.to_decoded().unwrap();
-    let utf8: String = String::from_utf8(decoded).unwrap();
-    let buildid = BuildID::from_vdf_steamcmd_contaminated(&utf8).unwrap();
+#[cfg(test)]
+mod test {
+    trait DecodeHex {
+        fn to_decoded(&self) -> Result<Vec<u8>, String>;
+    }
+
+    impl DecodeHex for &str {
+        fn to_decoded(&self) -> Result<Vec<u8>, String> {
+            self.trim()
+                .as_bytes()
+                .chunks(2)
+                .map(|chunk| {
+                    let hex_byte: &str = match std::str::from_utf8(chunk) {
+                        Ok(n) => n,
+                        Err(err) => return Err(err.to_string()),
+                    };
+                    match u8::from_str_radix(hex_byte, 16) {
+                        Ok(n) => Ok(n),
+                        Err(err) => Err(err.to_string()),
+                    }
+                })
+                .collect()
+        }
+    }
+
+    #[test]
+    fn from_vdf_steamcmd_contaminated() {
+        let hex: &'static str = include_str!("../../TODO/sample.hex");
+        let decoded: Vec<u8> = hex.to_decoded().unwrap();
+        let utf8: String = String::from_utf8(decoded).unwrap();
+        let _buildid = crate::steam::BuildID::from_vdf_steamcmd_contaminated(&utf8).unwrap();
+    }
 }
