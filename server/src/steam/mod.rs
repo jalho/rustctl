aa21165964dc648fce748a015a999f5e29e14917
jalho <alho.jere@gmail.com>
2025-11-02 14:24:03 +0200

    Modularize steam::test

--- server/src/steam/mod.rs
+++ server/src/steam/mod.rs
@@ -1,5 +1,8 @@
 //! Utilities for working with Steam or apps distributed via Steam.
 
+#[cfg(test)]
+mod test;
+
 pub struct RustDedicated;
 
 impl RustDedicated {
@@ -640,39 +643,5 @@ trait DisplayHex {
 impl DisplayHex for Vec<u8> {
     fn to_hex_encoded_string(&self) -> String {
         self.iter().map(|byte| format!("{:02x}", byte)).collect()
-    }
-}
-
-#[cfg(test)]
-mod test {
-    trait DecodeHex {
-        fn to_decoded(&self) -> Result<Vec<u8>, String>;
-    }
-
-    impl DecodeHex for &str {
-        fn to_decoded(&self) -> Result<Vec<u8>, String> {
-            self.trim()
-                .as_bytes()
-                .chunks(2)
-                .map(|chunk| {
-                    let hex_byte: &str = match std::str::from_utf8(chunk) {
-                        Ok(n) => n,
-                        Err(err) => return Err(err.to_string()),
-                    };
-                    match u8::from_str_radix(hex_byte, 16) {
-                        Ok(n) => Ok(n),
-                        Err(err) => Err(err.to_string()),
-                    }
-                })
-                .collect()
-        }
-    }
-
-    #[test]
-    fn from_vdf_steamcmd_contaminated() {
-        let hex: &'static str = include_str!("../../TODO/sample.hex");
-        let decoded: Vec<u8> = hex.to_decoded().unwrap();
-        let utf8: String = String::from_utf8(decoded).unwrap();
-        let _buildid = crate::steam::BuildID::from_vdf_steamcmd_contaminated(&utf8).unwrap();
     }
 }
