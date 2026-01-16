029d0e7a62df9f2132e5e21419dad5327d49f709
Jere Alho <alho.jere@gmail.com>
2026-01-17 01:30:13 +0200

    Add FifoPair

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -54,39 +54,77 @@ struct RtDone;
 mod game {
     pub const GAME_SERVER_FIFO_OUT: &str = "/tmp/rustctl/game-server.out";
     pub const GAME_SERVER_FIFO_ERR: &str = "/tmp/rustctl/game-server.err";
+    const FIFO_DIR: &str = "/tmp/rustctl";
+
+    struct FifoPair {
+        stdout: tokio::fs::File,
+        stderr: tokio::fs::File,
+    }
+
+    impl FifoPair {
+        async fn prepare_filesystem() {
+            tokio::fs::create_dir_all(FIFO_DIR).await.unwrap();
+
+            for path in [GAME_SERVER_FIFO_OUT, GAME_SERVER_FIFO_ERR] {
+                if !std::path::Path::new(path).exists() {
+                    match nix::unistd::mkfifo(
+                        path,
+                        nix::sys::stat::Mode::S_IRUSR | nix::sys::stat::Mode::S_IWUSR,
+                    ) {
+                        Ok(_) => log::debug!("Created FIFO: {}", path),
+                        Err(nix::errno::Errno::EEXIST) => {}
+                        Err(e) => panic!("Failed to create FIFO {}: {}", path, e),
+                    }
+                }
+            }
+        }
+
+        async fn open_with_options(options: &tokio::fs::OpenOptions) -> Self {
+            Self::prepare_filesystem().await;
+            Self {
+                stdout: options.open(GAME_SERVER_FIFO_OUT).await.unwrap(),
+                stderr: options.open(GAME_SERVER_FIFO_ERR).await.unwrap(),
+            }
+        }
+
+        async fn open_for_reading() -> Self {
+            Self::open_with_options(tokio::fs::OpenOptions::new().read(true)).await
+        }
+
+        async fn open_for_writing() -> Self {
+            Self::open_with_options(tokio::fs::OpenOptions::new().write(true)).await
+        }
+
+        async fn open_dummy_handles() -> Self {
+            let mut options = tokio::fs::OpenOptions::new();
+            options.read(true).custom_flags(nix::libc::O_NONBLOCK);
+            Self::open_with_options(&options).await
+        }
+    }
 
     pub async fn log_game_server_output() {
         loop {
-            let out_fifo: tokio::fs::File = tokio::fs::File::open(GAME_SERVER_FIFO_OUT)
-                .await
-                .unwrap();
-            let err_fifo: tokio::fs::File = tokio::fs::File::open(GAME_SERVER_FIFO_ERR)
-                .await
-                .unwrap();
+            let fifos = FifoPair::open_for_reading().await;
 
-            let out_reader: tokio::io::BufReader<tokio::fs::File> =
-                tokio::io::BufReader::new(out_fifo);
-            let err_reader: tokio::io::BufReader<tokio::fs::File> =
-                tokio::io::BufReader::new(err_fifo);
+            let out_reader = tokio::io::BufReader::new(fifos.stdout);
+            let err_reader = tokio::io::BufReader::new(fifos.stderr);
 
-            let mut out_lines: tokio::io::Lines<tokio::io::BufReader<tokio::fs::File>> =
-                tokio::io::AsyncBufReadExt::lines(out_reader);
-            let mut err_lines: tokio::io::Lines<tokio::io::BufReader<tokio::fs::File>> =
-                tokio::io::AsyncBufReadExt::lines(err_reader);
+            let mut out_lines = tokio::io::AsyncBufReadExt::lines(out_reader);
+            let mut err_lines = tokio::io::AsyncBufReadExt::lines(err_reader);
 
             loop {
                 tokio::select! {
                     res = out_lines.next_line() => {
                         match res {
                             Ok(Some(line)) => log::debug!("[STDOUT] {}", line),
-                            // EOF reached: break inner loop to re-open FIFOs
+                            /* EOF reached: break inner loop to re-open FIFOs */
                             _ => break,
                         }
                     }
                     res = err_lines.next_line() => {
                         match res {
                             Ok(Some(line)) => log::debug!("[STDERR] {}", line),
-                            // EOF reached: break inner loop to re-open FIFOs
+                            /* EOF reached: break inner loop to re-open FIFOs */
                             _ => break,
                         }
                     }
@@ -97,45 +135,22 @@ mod game {
         }
     }
 
-    async fn open_dummy_handles() -> (std::fs::File, std::fs::File) {
-        use std::os::unix::fs::OpenOptionsExt;
-        let keep_stdout_open: std::fs::File = std::fs::OpenOptions::new()
-            .read(true)
-            .custom_flags(nix::libc::O_NONBLOCK)
-            .open(GAME_SERVER_FIFO_OUT)
-            .unwrap();
-        let keep_stderr_open: std::fs::File = std::fs::OpenOptions::new()
-            .read(true)
-            .custom_flags(nix::libc::O_NONBLOCK)
-            .open(GAME_SERVER_FIFO_ERR)
-            .unwrap();
-        (keep_stdout_open, keep_stderr_open)
-    }
-
-    async fn open_fifos() -> (std::fs::File, std::fs::File) {
-        let out_file: std::fs::File = std::fs::OpenOptions::new()
-            .write(true)
-            .open(GAME_SERVER_FIFO_OUT)
-            .unwrap();
-        let err_file: std::fs::File = std::fs::OpenOptions::new()
-            .write(true)
-            .open(GAME_SERVER_FIFO_ERR)
-            .unwrap();
-        (out_file, err_file)
-    }
-
     pub async fn spawn(executable: &str) -> () {
         /*
          * Keep a read-end open for both FIFOs. This ensures that even if
          * the Service restarts, the Game process always sees at least one
          * reader and thus doesn't terminate when readers drop to 0.
          */
-        let _dummies = open_dummy_handles().await;
+        let _dummies = FifoPair::open_dummy_handles().await;
 
-        let (out_file, err_file) = open_fifos().await;
+        let fifos = FifoPair::open_for_writing().await;
+
+        let stdout_std = fifos.stdout.into_std().await;
+        let stderr_std = fifos.stderr.into_std().await;
+
         let mut child: tokio::process::Child = tokio::process::Command::new(executable)
-            .stdout(std::process::Stdio::from(out_file))
-            .stderr(std::process::Stdio::from(err_file))
+            .stdout(stdout_std)
+            .stderr(stderr_std)
             .spawn()
             .expect("Failed to spawn game server");
         let _status: std::process::ExitStatus = child.wait().await.unwrap();
