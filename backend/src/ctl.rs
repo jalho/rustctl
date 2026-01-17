3a63f12d03ee1f390fc8fef47ae90a8803f5b4cf
jalho <alho.jere@gmail.com>
2026-01-17 16:27:08 +0200

    Reboot with systemctl

--- backend/src/ctl.rs
+++ backend/src/ctl.rs
@@ -6,12 +6,20 @@ pub async fn handle_commands_from_web_clients(mut rx: tokio::sync::mpsc::Receive
     loop {
         if let Some(n) = rx.recv().await {
             match n {
-                Command::Reboot => reboot().await,
+                Command::Reboot => reboot_using_systemctl().await,
             }
         }
     }
 }
 
-async fn reboot() {
-    log::debug!("TODO: Reboot");
+async fn reboot_using_systemctl() {
+    let status = tokio::process::Command::new("systemctl")
+        .arg("reboot")
+        .status()
+        .await
+        .unwrap();
+
+    if !status.success() {
+        log::error!("Failed to reboot: systemctl exited with status code: {status}");
+    }
 }
