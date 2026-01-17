754d394e3f8fdf396081972bc15b3add3e337555
jalho <alho.jere@gmail.com>
2026-01-17 16:44:13 +0200

    Only reboot in cloud

--- backend/src/ctl.rs
+++ backend/src/ctl.rs
@@ -13,6 +13,13 @@ pub async fn handle_commands_from_web_clients(mut rx: tokio::sync::mpsc::Receive
 }
 
 async fn reboot_using_systemctl() {
+    if let Ok(metadata) = is_hetzner_vm().await {
+        log::info!("Rebooting cloud instance: {metadata}");
+    } else {
+        log::info!("Skipping reboot: Not in cloud");
+        return;
+    }
+
     let status = tokio::process::Command::new("systemctl")
         .arg("reboot")
         .status()
@@ -22,4 +29,26 @@ async fn reboot_using_systemctl() {
     if !status.success() {
         log::error!("Failed to reboot: systemctl exited with status code: {status}");
     }
+}
+
+async fn is_hetzner_vm() -> Result<String, reqwest::Error> {
+    /*
+     * Docs: https://docs.hetzner.cloud/reference/cloud
+     *       Accessed 2026-01-17.
+     */
+    let url: &'static str = "http://169.254.169.254/hetzner/v1/metadata";
+
+    let client: reqwest::Client = reqwest::Client::builder()
+        .timeout(std::time::Duration::from_millis(500))
+        .build()
+        .unwrap();
+
+    let response: reqwest::Response = match client.get(url).send().await {
+        Ok(n) => n,
+        Err(err) => return Err(err),
+    };
+
+    let instance_metadata: String = response.text().await.unwrap();
+
+    Ok(instance_metadata)
 }
