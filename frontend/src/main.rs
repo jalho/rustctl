334c19004cd455fd6ee7d0aff90816fcc852e209
jalho <alho.jere@gmail.com>
2026-01-18 16:53:07 +0200

    Remove bloat

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -10,24 +10,6 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        Hero {}
-    }
-}
-
-#[component]
-pub fn Hero() -> Element {
-    rsx! {
-        div { id: "hero",
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                    "💫 VSCode Extension"
-                }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
-        }
+        p { "Hello world!" }
     }
 }
