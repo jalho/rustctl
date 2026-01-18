4f13ab12ec1f0ea30a4d1bfaad6d0877ed0d170b
jalho <alho.jere@gmail.com>
2026-01-18 17:03:48 +0200

    Declare modules explicitly

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,9 +1,14 @@
-use dioxus::prelude::*;
+use dioxus::dioxus_core;
+use dioxus::document;
+use dioxus::prelude::asset;
+use dioxus::prelude::dioxus_elements;
+use dioxus::prelude::dioxus_signals;
+use dioxus::prelude::manganis;
 
-#[component]
-pub fn App() -> Element {
-    rsx! {
+#[dioxus::prelude::component]
+pub fn App() -> dioxus::core::Element {
+    dioxus::prelude::rsx! {
         document::Link { rel: "stylesheet", href: asset!("/assets/main.css") }
-        p { "Hello world!" }
+        p { "Hello, world!" }
     }
 }
