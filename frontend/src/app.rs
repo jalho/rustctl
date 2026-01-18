8af8feed3b73cfad594e07eb203cd686f6193e27
jalho <alho.jere@gmail.com>
2026-01-18 17:24:07 +0200

    Connect WebSocket

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -7,8 +7,20 @@ use dioxus::prelude::manganis;
 
 #[dioxus::prelude::component]
 pub fn App() -> dioxus::core::Element {
+    dioxus::prelude::use_future(connect_websocket);
+
     dioxus::prelude::rsx! {
         document::Link { rel: "stylesheet", href: asset!("/assets/main.css") }
         p { "Hello, world!" }
+    }
+}
+
+async fn connect_websocket() {
+    loop {
+        if let Ok(mut socket) = gloo_net::websocket::futures::WebSocket::open("/websocket") {
+            use futures_util::StreamExt;
+            while (socket.next().await).is_some() {}
+        }
+        gloo_timers::future::sleep(std::time::Duration::from_secs(1)).await;
     }
 }
