b16c344ec00373f452dfb088fe064a7740fd7f15
jalho <alho.jere@gmail.com>
2026-01-18 18:36:25 +0200

    Separate concerns

--- frontend/src/state.rs
+++ frontend/src/state.rs
@@ -11,16 +11,35 @@ impl GlobalState {
     }
 
     pub async fn connect_websocket() {
-        let mut state = dioxus::hooks::use_context::<GlobalState>();
-        loop {
-            dioxus::signals::WritableExt::with_mut(&mut state.connection_attempts, |count| {
-                *count += 1
-            });
-            if let Ok(mut socket) = gloo_net::websocket::futures::WebSocket::open("/websocket") {
-                use futures_util::StreamExt;
-                while (socket.next().await).is_some() {}
-            }
+        let mut state: GlobalState = dioxus::hooks::use_context::<GlobalState>();
+
+        'reconnect: loop {
+            dioxus::signals::WritableExt::with_mut(&mut state.connection_attempts, |n| *n += 1);
+
+            let socket: Socket = match gloo_net::websocket::futures::WebSocket::open("/websocket") {
+                Ok(n) => n,
+                Err(_) => continue 'reconnect,
+            };
+
+            Self::handle_socket_receive_messages(socket).await;
+
             gloo_timers::future::sleep(std::time::Duration::from_secs(1)).await;
         }
     }
+
+    async fn handle_socket_receive_messages(mut socket: Socket) {
+        use futures_util::StreamExt;
+
+        match socket.next().await {
+            Some(Ok(n)) => {
+                let _n: gloo_net::websocket::Message = n;
+            }
+            Some(Err(err)) => {
+                let _err: gloo_net::websocket::WebSocketError = err;
+            }
+            None => return,
+        }
+    }
 }
+
+type Socket = gloo_net::websocket::futures::WebSocket;
