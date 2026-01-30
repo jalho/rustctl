0640b04227111aacae5bfe734310220142055426
jalho <alho.jere@gmail.com>
2026-01-30 17:57:22 +0200

    Add route POST /auth/init

--- backend/src/web.rs
+++ backend/src/web.rs
@@ -25,8 +25,9 @@ pub async fn serve<A: tokio::net::ToSocketAddrs>(
     /*
      * Logic routes.
      *
-     * TODO: Add access control to the logic routes.
+     * TODO: Add access control to some of the logic routes (post-auth).
      */
+    router = router.route("/auth/init", axum::routing::post(handle_auth_init));
     router = router.route("/reboot", axum::routing::post(handle_reboot));
 
     let router: axum::Router = router.with_state(State::new(tx));
@@ -67,13 +68,91 @@ async fn handle_reboot(
     axum::response::Response::new(body)
 }
 
+async fn handle_auth_init(
+    axum::extract::State(state): axum::extract::State<State>,
+) -> axum::response::Json<RegistrationOptions> {
+    let mut challenge_bytes: [u8; 32] = [0u8; 32];
+    {
+        let mut generator: rand::prelude::ThreadRng = rand::rng();
+        use rand::RngCore;
+        generator.fill_bytes(&mut challenge_bytes);
+    }
+
+    let challenge_b64: String = <base64::engine::GeneralPurpose as base64::Engine>::encode(
+        &base64::engine::general_purpose::URL_SAFE_NO_PAD,
+        challenge_bytes,
+    );
+
+    {
+        let mut lock = state.pending_challenges.lock().await;
+        lock.insert(challenge_b64.clone());
+    }
+
+    let options: RegistrationOptions = RegistrationOptions {
+        challenge: challenge_b64,
+        rp: Rp {
+            name: "PLACEHOLDER1".into(),
+            id: "rustctl.internal".into(), // TODO: Use a public domain name
+        },
+        user: User {
+            id: "PLACEHOLDER2".into(),
+            name: "PLACEHOLDER3".into(),
+            display_name: "PLACEHOLDER4".into(),
+        },
+        pub_key_cred_params: vec![PubKeyCredParam {
+            alg: -7, // "ECDSA using P-256 and SHA-256"
+            kind: String::from("public-key"),
+        }],
+        timeout: 60000, // milliseconds
+    };
+
+    axum::response::Json(options)
+}
+
+#[derive(serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RegistrationOptions {
+    challenge: String,
+    rp: Rp,
+    user: User,
+    pub_key_cred_params: Vec<PubKeyCredParam>,
+    timeout: u64,
+}
+
+#[derive(serde::Serialize)]
+struct Rp {
+    name: String,
+    id: String,
+}
+
+#[derive(serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct User {
+    id: String,
+    name: String,
+    display_name: String,
+}
+
+#[derive(serde::Serialize)]
+struct PubKeyCredParam {
+    alg: i32,
+    #[serde(rename = "type")]
+    kind: String,
+}
+
 #[derive(Clone)]
 struct State {
     tx: tokio::sync::mpsc::Sender<crate::ctl::Command>,
+    pending_challenges: std::sync::Arc<tokio::sync::Mutex<std::collections::HashSet<String>>>,
 }
 
 impl State {
     fn new(tx: tokio::sync::mpsc::Sender<crate::ctl::Command>) -> Self {
-        Self { tx }
+        Self {
+            tx,
+            pending_challenges: std::sync::Arc::new(tokio::sync::Mutex::new(
+                std::collections::HashSet::new(),
+            )),
+        }
     }
 }
