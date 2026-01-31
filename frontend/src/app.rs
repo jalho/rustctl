1369df9eee06fbabc1e8535274030f44067e6b7f
Jere Alho <alho.jere@gmail.com>
2026-01-31 16:48:37 +0200

    Regenerate

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -71,60 +71,47 @@ pub async fn handle_sign_up() {
 
     let pk = &resp.ccr["publicKey"];
 
-    // Decode Challenge
     let challenge_raw = pk["challenge"]
         .as_str()
         .unwrap_or_else(|| log_error_and_panic("Challenge missing"));
-    let challenge_base64 = challenge_raw.replace('-', "+").replace('_', "/");
-    let window = web_sys::window().unwrap_or_else(|| log_error_and_panic("No window"));
-    let decoded_str = window
-        .atob(&challenge_base64)
-        .unwrap_or_else(|_| log_error_and_panic("atob failed"));
-    let challenge_js = js_sys::Uint8Array::new_with_length(decoded_str.len() as u32);
-    for (i, byte) in decoded_str.bytes().enumerate() {
-        challenge_js.set_index(i as u32, byte);
-    }
 
-    // Entities
-    let rp_entity = web_sys::PublicKeyCredentialRpEntity::new(
-        pk["rp"]["id"]
-            .as_str()
-            .unwrap_or_else(|| log_error_and_panic("rp.id missing")),
-    );
-    rp_entity.set_name(
-        pk["rp"]["name"]
-            .as_str()
-            .unwrap_or_else(|| log_error_and_panic("rp.name missing")),
-    );
+    let challenge_bytes =
+        base64::Engine::decode(&base64::prelude::BASE64_URL_SAFE_NO_PAD, challenge_raw)
+            .unwrap_or_else(|e| {
+                log_error_and_panic(&format!("Base64URL challenge decode failed: {:?}", e))
+            });
 
-    let user_id_js = js_sys::Uint8Array::from(
-        pk["user"]["id"]
-            .as_str()
-            .unwrap_or_else(|| log_error_and_panic("user.id missing"))
-            .as_bytes(),
-    );
+    let challenge_js = js_sys::Uint8Array::from(&challenge_bytes[..]);
+
+    let user_id_raw = pk["user"]["id"]
+        .as_str()
+        .unwrap_or_else(|| log_error_and_panic("User ID missing"));
+
+    let user_id_bytes =
+        base64::Engine::decode(&base64::prelude::BASE64_URL_SAFE_NO_PAD, user_id_raw)
+            .unwrap_or_else(|_| user_id_raw.as_bytes().to_vec());
+
+    let user_id_js = js_sys::Uint8Array::from(&user_id_bytes[..]);
+
+    let rp_entity = web_sys::PublicKeyCredentialRpEntity::new(pk["rp"]["id"].as_str().unwrap());
+    rp_entity.set_name(pk["rp"]["name"].as_str().unwrap());
+
     let user_entity = web_sys::PublicKeyCredentialUserEntity::new(
-        pk["user"]["name"]
-            .as_str()
-            .unwrap_or_else(|| log_error_and_panic("user.name missing")),
-        pk["user"]["displayName"]
-            .as_str()
-            .unwrap_or_else(|| log_error_and_panic("user.displayName missing")),
+        pk["user"]["name"].as_str().unwrap(),
+        pk["user"]["displayName"].as_str().unwrap(),
         &user_id_js,
     );
 
-    // Params: Fix for "Cannot convert a BigInt value to a number"
     let params_array = js_sys::Array::new();
-    let params_json = pk["pubKeyCredParams"]
-        .as_array()
-        .unwrap_or_else(|| log_error_and_panic("pubKeyCredParams missing"));
-    for p in params_json {
-        let obj = js_sys::Object::new();
-        js_sys::Reflect::set(&obj, &"type".into(), &p["type"].as_str().unwrap().into()).unwrap();
-        // FIX: Cast i64 to i32 before converting to JsValue to avoid BigInt issues
-        let alg_i32 = p["alg"].as_i64().unwrap() as i32;
-        js_sys::Reflect::set(&obj, &"alg".into(), &alg_i32.into()).unwrap();
-        params_array.push(&obj);
+    if let Some(params_json) = pk["pubKeyCredParams"].as_array() {
+        for p in params_json {
+            let obj = js_sys::Object::new();
+            js_sys::Reflect::set(&obj, &"type".into(), &p["type"].as_str().unwrap().into())
+                .unwrap();
+            let alg = p["alg"].as_i64().unwrap() as i32;
+            js_sys::Reflect::set(&obj, &"alg".into(), &alg.into()).unwrap();
+            params_array.push(&obj);
+        }
     }
 
     let options = web_sys::PublicKeyCredentialCreationOptions::new(
@@ -133,65 +120,92 @@ pub async fn handle_sign_up() {
         &rp_entity,
         &user_entity,
     );
+
     let auth_selection = web_sys::AuthenticatorSelectionCriteria::new();
     auth_selection.set_require_resident_key(
         pk["authenticatorSelection"]["requireResidentKey"]
             .as_bool()
-            .unwrap(),
+            .unwrap_or(false),
     );
     auth_selection.set_user_verification(web_sys::UserVerificationRequirement::Required);
     options.set_authenticator_selection(&auth_selection);
-    options.set_timeout(pk["timeout"].as_f64().unwrap() as u32);
+
+    if let Some(timeout) = pk["timeout"].as_f64() {
+        options.set_timeout(timeout as u32);
+    }
 
     let create_options = web_sys::CredentialCreationOptions::new();
     create_options.set_public_key(&options);
 
+    let window = web_sys::window().unwrap();
     let promise = window
         .navigator()
         .credentials()
         .create_with_options(&create_options)
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Credentials.create failed: {:?}", e)));
-
+        .unwrap();
     let result = wasm_bindgen_futures::JsFuture::from(promise)
         .await
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Platform error/User cancel: {:?}", e)));
+        .unwrap_or_else(|e| log_error_and_panic(&format!("Passkey creation failed: {:?}", e)));
 
     let submit_url = shared::SIGN_UP_SUBMIT.replace("{challenge_id}", &resp.id.to_string());
     gloo_net::http::Request::post(&submit_url)
         .json(&credential_to_json(result))
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Submit serialization failed: {:?}", e)))
+        .unwrap()
         .send()
         .await
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Submit POST failed: {:?}", e)));
+        .unwrap_or_else(|e| log_error_and_panic(&format!("Submission failed: {:?}", e)));
 }
 
 pub async fn handle_sign_in() {
     let resp: serde_json::Value = gloo_net::http::Request::post(shared::SIGN_IN_CHALLENGE)
         .send()
         .await
-        .unwrap_or_else(|e| log_error_and_panic(&format!("SignIn challenge failed: {:?}", e)))
+        .unwrap_or_else(|e| log_error_and_panic(&format!("POST challenge failed: {:?}", e)))
         .json()
         .await
-        .unwrap_or_else(|e| log_error_and_panic(&format!("SignIn JSON parse failed: {:?}", e)));
+        .unwrap_or_else(|e| log_error_and_panic(&format!("Parse SignInResponse failed: {:?}", e)));
 
-    // Handle Hex decoding (keeping your logic from app.rs)
-    let challenge_hex = resp["challenge"]
+    let challenge_raw = resp["publicKey"]["challenge"]
         .as_str()
         .unwrap_or_else(|| log_error_and_panic("SignIn challenge missing"));
-    let challenge_js = js_sys::Uint8Array::new_with_length((challenge_hex.len() / 2) as u32);
-    for i in 0..(challenge_hex.len() / 2) {
-        let byte = u8::from_str_radix(&challenge_hex[i * 2..i * 2 + 2], 16)
-            .unwrap_or_else(|_| log_error_and_panic("Invalid hex in challenge"));
-        challenge_js.set_index(i as u32, byte);
-    }
+
+    let challenge_bytes =
+        base64::Engine::decode(&base64::prelude::BASE64_URL_SAFE_NO_PAD, challenge_raw)
+            .unwrap_or_else(|e| {
+                log_error_and_panic(&format!("Base64URL challenge decode failed: {:?}", e))
+            });
+
+    let challenge_js = js_sys::Uint8Array::from(&challenge_bytes[..]);
 
     let options = web_sys::PublicKeyCredentialRequestOptions::new(&challenge_js);
-    options.set_timeout(60000);
-    if let Some(rp_id) = resp["rpId"].as_str() {
+
+    if let Some(timeout) = resp["publicKey"]["timeout"].as_f64() {
+        options.set_timeout(timeout as u32);
+    }
+
+    if let Some(rp_id) = resp["publicKey"]["rpId"].as_str() {
         options.set_rp_id(rp_id);
     }
 
-    let window = web_sys::window().unwrap_or_else(|| log_error_and_panic("No window"));
+    if let Some(allow_credentials) = resp["publicKey"]["allowCredentials"].as_array() {
+        let allow_creds_array = js_sys::Array::new();
+        for cred in allow_credentials {
+            let obj = js_sys::Object::new();
+            js_sys::Reflect::set(&obj, &"type".into(), &cred["type"].as_str().unwrap().into())
+                .unwrap();
+
+            let id_raw = cred["id"].as_str().unwrap();
+            let id_bytes = base64::Engine::decode(&base64::prelude::BASE64_URL_SAFE_NO_PAD, id_raw)
+                .unwrap_or_else(|_| id_raw.as_bytes().to_vec());
+            let id_js = js_sys::Uint8Array::from(&id_bytes[..]);
+            js_sys::Reflect::set(&obj, &"id".into(), &id_js.into()).unwrap();
+
+            allow_creds_array.push(&obj);
+        }
+        options.set_allow_credentials(&allow_creds_array);
+    }
+
+    let window = web_sys::window().unwrap();
     let req_options = web_sys::CredentialRequestOptions::new();
     req_options.set_public_key(&options);
 
@@ -199,16 +213,16 @@ pub async fn handle_sign_in() {
         .navigator()
         .credentials()
         .get_with_options(&req_options)
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Credentials.get failed: {:?}", e)));
+        .unwrap();
 
-    let credential = wasm_bindgen_futures::JsFuture::from(promise)
+    let result = wasm_bindgen_futures::JsFuture::from(promise)
         .await
         .unwrap_or_else(|e| log_error_and_panic(&format!("SignIn platform error: {:?}", e)));
 
     gloo_net::http::Request::post(shared::SIGN_IN_SUBMIT)
-        .json(&credential_to_json(credential))
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Submit serialization failed: {:?}", e)))
+        .json(&credential_to_json(result))
+        .unwrap()
         .send()
         .await
-        .unwrap_or_else(|e| log_error_and_panic(&format!("Submit POST failed: {:?}", e)));
+        .unwrap_or_else(|e| log_error_and_panic(&format!("SignIn submission failed: {:?}", e)));
 }
