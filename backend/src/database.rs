f280b23abb39b1262d8651a332512e20618ee0c7
Jere Alho <alho.jere@gmail.com>
2026-01-31 19:49:22 +0200

    Debug

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -25,6 +25,10 @@ impl Client {
         &mut self,
         credential_id: &webauthn_rs::prelude::CredentialID,
     ) -> Option<webauthn_rs::prelude::Passkey> {
+        log::debug!(
+            "Checking credential ID against {count} known passkeys",
+            count = self.in_mem.len(),
+        );
         for v in self.in_mem.iter() {
             let deserialized: webauthn_rs::prelude::Passkey = serde_json::from_str(v).unwrap();
             let id: &webauthn_rs::prelude::CredentialID = deserialized.cred_id();
