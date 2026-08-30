02f662aa35d80461fc9d39e5cdb634ad627f6b46
jalho <alho.jere@gmail.com>
2026-08-30 12:09:37 +0300

    Describe intended architecture

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,13 @@
 //! a web server for a web app for managing the game server. Each should be
 //! independently restartable. Both are defined in the same code base, distinguished
 //! at startup by subcommand given via command line interface.
+//!
+//! The game server gives information of itself to the managing web server via a
+//! Unix domain socket. The game (_Rust_) is instrumented with a modding framework
+//! (_Carbon_), for which we define a plugin that writes information about the game
+//! server's state into the Unix domain socket that the managing web server then
+//! reads. The modding framework takes care of detecting changes in the game's state
+//! and passing the information to the plugin.
 
 fn main() -> std::process::ExitCode {
     let cli: cli::Cli = <cli::Cli as clap::Parser>::parse();
